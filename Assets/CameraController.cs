70c8b2a8c878a1a1bdb504185bdf147691836fc8
David Phillips <davidphillips000@gmail.com>
2016-12-23 23:07:51 -0600

    cube rotates based on wasd keys, webcam text activate camera and takes pic, click on cube to apply

---
+++ Assets/CameraController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class CameraController : MonoBehaviour
+{
+	public WebCamTexture mCamera = null;
+	public GameObject plane,plane2;
+	public Texture2D testTexture;
+	// Use this for initialization
+
+	WebCamTexture _CamTex;
+	///private string _SavePath = Application.dataPath;
+	int _CaptureCounter = 0;
+
+
+	public Texture2D TakePic(){
+
+		//Texture2D PhotoTaken= new Texture2D(mCamera.width, mCamera.height);
+		Texture2D PhotoTaken= new Texture2D(mCamera.width, mCamera.height);
+
+		PhotoTaken.SetPixels(mCamera.GetPixels());
+		PhotoTaken.Apply();
+
+		return PhotoTaken;
+
+	}
+	void Update(){
+		//add sides array of pics
+		//auto rotate to correct side, wait for click to load pic on tht side
+		//arrow keys move cube
+		if(Input.GetKey(KeyCode.Space)){
+			StopAllCoroutines();
+		
+			//Texture2D PhotoTaken= new Texture2D(mCamera.width, mCamera.height);
+			Texture2D PhotoTaken= new Texture2D(mCamera.width, mCamera.height);
+
+			PhotoTaken.SetPixels(mCamera.GetPixels());
+			PhotoTaken.Apply();
+
+			mCamera.Stop ();
+
+			plane2.GetComponent<Renderer>().material.mainTexture = mCamera;
+
+			//var bytes1 = testTexture.EncodeToPNG ();
+			var bytes1 = PhotoTaken.EncodeToPNG ();
+			var path = System.IO.Path.Combine("Assets/Resources/" , "gloopity gloop" + ".png");
+			System.IO.File.WriteAllBytes (path, bytes1);
+			Debug.Log(path);
+
+		}
+	}
+
+
+	IEnumerator Start ()
+	{
+
+		//SaveTextureToFile(testTexture, "wooo");
+		//TakeSnapshot();
+		mCamera = new WebCamTexture ();
+		plane.GetComponent<Renderer>().material.mainTexture = mCamera;
+
+		mCamera.Play ();
+		if(mCamera.width <= 16){ 
+			while(!mCamera.didUpdateThisFrame){ 
+				yield return new WaitForEndOfFrame(); 
+			} 
+			mCamera.Pause(); 
+			Color32[] colors = mCamera.GetPixels32(); 
+			mCamera.Stop();
+
+			yield return new WaitForEndOfFrame();
+
+			mCamera.Play(); 
+		}
+	}
+}
---
+++ Assets/MoveCube.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCube : MonoBehaviour {
+
+	public float speed = 1;
+	private GameObject endRotation;
+
+	void Start(){
+		endRotation = new GameObject();
+	}
+
+
+
+	void Update () {
+
+//		float dot = Vector3.Dot(transform.forward, (Camera.main.transform.position - transform.position).normalized);
+//		if(dot > 0.7f) { Debug.Log("Quite facing");}
+
+		//iterating through the faces:
+
+		//faceNormal[i].dot(-worldViewVector) > 0.8 //(should actually be >0.9999)
+
+
+		//Take the dot-product of the view-vector (imagine an arrow coming out of the camera) and the surface normal. If it's negative, then the face is visible.
+
+		if( Input.GetKeyDown( KeyCode.RightArrow ) ){
+			endRotation.transform.Rotate(Vector3.forward, 90, Space.World);
+		}
+		if( Input.GetKeyDown( KeyCode.LeftArrow ) ){
+			endRotation.transform.Rotate(Vector3.forward, -90, Space.World);
+		}
+
+
+		if( Input.GetKeyDown( "a" ) ){
+			endRotation.transform.Rotate(Vector3.up, 90, Space.World);
+		}
+		if( Input.GetKeyDown( "d" ) ){
+			endRotation.transform.Rotate(Vector3.up, -90, Space.World);
+		}
+
+
+		if( Input.GetKeyDown( "w" ) ){
+			endRotation.transform.Rotate(Vector3.left, 90, Space.World);
+		}
+		if( Input.GetKeyDown( "s") ){
+			endRotation.transform.Rotate(Vector3.left, -90, Space.World);
+		}
+
+		this.transform.rotation = Quaternion.Lerp(this.transform.rotation, endRotation.transform.rotation, Time.deltaTime*speed);
+
+	}
+}
